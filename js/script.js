bba9bcc9d0875ea73d887dfa11c9adf82bda738e
Dawid Kowalski <dawid.k.kowalski@gmail.com>
2018-12-13 16:24:32 +0100

    Add function to create and add hamster to array

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
+let hamsterNameHTML = document.getElementById("hamster-name");
+let hamsterAgeHTML = document.getElementById("hamster-age");
+let hamsterTypeHTML = document.getElementById("hamster-type");
+
+let cage = [];
+
 function Hamster(name, age, type) {
 	this.name = name;
 	this.age = age;
 	this.type = type;
 }
+
+function goToCage() {
+	let hamsterName = document.getElementById("hamster-name").value;
+	let hamsterAge = document.getElementById("hamster-age").value;
+	let hamsterType = document.getElementById("hamster-type").value;
+
+	let hamster = new Hamster(hamsterName, hamsterAge, hamsterType);
+
+	cage.push(hamster);
+}
+
